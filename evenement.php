c9600eb07464599a67e6f2203304ce77ef38be2e
Caporal6 <xavier.2@hotmail.ca>
2023-09-25 18:41:59 -0400

    lol

--- evenement.php
+++ evenement.php
@@ -215,6 +215,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
     ?>
 
+<?php
+  // Set session variables
+    $_SESSION["connexion"] = true; 
+    echo "La connexion est réussie" . $_SESSION["connexion"];
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
