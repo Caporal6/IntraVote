e771911757266f5b416f4e0824c0c204848b640f
Caporal6 <xavier.2@hotmail.ca>
2023-09-25 20:03:01 -0400

    bruh

--- vote.php
+++ vote.php
@@ -56,8 +56,62 @@ else{
     <h3><em>Boff:<?php echo $_SESSION['attnumBoff'] ?>: </em></h3>
     <h3><em>Mauvais:<?php echo $_SESSION['attnumMauvais'] ?>: </em></h3>
 
+    <a href="evenement.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Evenement.php</a> 
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <tr>
+        
+                        <th scope="col">#id</th>
+                        <th scope="col">vote_etudiant</th>
+                        <th scope="col">vote_employeur</th>
+                        <th scope="col">bon</th>
+                        <th scope="col">boff</th>
+                        <th scope="col">mauvais</th>
+  
+                    </tr>
+                </thead>
+                <tbody>
+    
+                    <?php 
+                    
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "Azgt3878";
+                        $db = "intra";
+
+                        // Create connection
+                        $conn = new mysqli($servername, $username, $password, $db);
+                        // Check connection
+
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        $conn->query('SET NAMES utf8'); $sql = "SELECT * FROM vote";
+                        $result = $conn->query($sql);
+                            if ($result->num_rows > 0) {
+                        // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                $var_value = $row["id"];
+                                echo '<tr><th scope="row">'. $row["id"].'</th><td>'. $row["vote_etudiant"].'</td><td>'. $row["vote_employeur"].'</td><td>'. $row["bon"].'</td>
+                                <td>'. $row["moyen"].'</td> <td>'. $row["mauvais"].'</td> 
+                                </tr>'; 
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        $conn->close();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
